e912bfd6c8bdf4c861a269104362dfaed4f09581
Ryan-Duffett <124389615+Ryan-Duffett@users.noreply.github.com>
2023-03-20 17:19:51 +0000

    added the script

--- js/script.js
+++ js/script.js
@@ -4,6 +4,15 @@
 // Created on: Sep 2020
 // This file contains the JS functions for index.html
 
-function buttonClicked() {
-alert("Hello, World!")
+/**
+ * This function gets users first name and age and shows it back to user.
+ */
+function enterClicked() {
+  // input
+  const firstName = document.getElementById("first-name").value
+  const userAge = parseInt(document.getElementById("age-entered").value)
+
+  // output
+  document.getElementById("address").innerHTML =
+    "Your address is: " + userAge + "  " + firstName + "."
 }
